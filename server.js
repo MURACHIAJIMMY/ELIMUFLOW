06c0ed513fa91e11f6bada5ea0cf8aa83abd3547
MURACHIA JIMMY <jamesmurachia84@gmail.com>
2025-11-15 14:10:01 +0300

    v10

--- server.js
+++ server.js
@@ -54,6 +54,8 @@ app.use('/api/schools', schoolRoutes);
 app.use('/api/admin', promotionRoute);
 app.use('/api', yearRoutes);
 // ✅ Server start
+console.log("Using Chromium from:", puppeteer.executablePath());
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}🚀🚀`));
 // ✅ Promotion scheduler
